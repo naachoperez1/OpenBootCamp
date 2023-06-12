9f0b448dbea775de100a59ec11b611c157892dcd
Ignacio Perez <85687161+naachoperez1@users.noreply.github.com>
2023-06-12 08:27:52 -0300

    added files
    added files for ej 3

---
+++ Main.java
@@ -0,0 +1,41 @@
+public class Main {
+    public static void main(String[] args) {
+
+        Persona persona1 = new Persona();
+        persona1.setNombre("Nacho");
+        persona1.setEdad(25);
+        persona1.setTelefono("3654985642");
+
+        System.out.println(persona1.nombre);
+        System.out.println(persona1.edad);
+        System.out.println(persona1.telefono);
+    }
+
+    public static class Persona{
+        private int edad;
+        private String nombre;
+        private String telefono;
+
+            public void setNombre(String nombre){
+                this.nombre = nombre;
+            }
+            public String getNombre(){
+                return this.nombre;
+            }
+
+            public void setEdad(int edad){
+                this.edad = edad;
+            }
+            public int getEdad(){
+                return this.edad;
+            }
+
+            public void setTelefono(String telefono){
+                this.telefono = telefono;
+            }
+            public String getTelefono(){
+                return this.telefono;
+            }
+    }
+}
+
